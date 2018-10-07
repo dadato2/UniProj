47f71a8d309ec20d6a2835b4ce6be9c257270777
dadato2 <dadato2@users.noreply.github.com>
2018-10-07 17:41:02 +0100

    problem solved

---
+++ Homework.py
@@ -0,0 +1,14 @@
+word = str(input('Give word ("stop" to exit): '))
+while word != 'stop':
+    if len(word) > 3:
+        if word[-1] == 'g' and word[-2] == 'n' and word[-3] == 'i':
+            word += 'ly'
+        elif word[-1] == 'e':
+            word = word[:-1]
+            word += 'ing'
+        else:
+            word += 'ing'
+    print(word)
+    word = str(input('Give word: '))
+
+

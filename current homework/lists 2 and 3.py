d08a4492da89b4cd969ee20819d78a5c38b1c7d1
dadato2 <dadato2@users.noreply.github.com>
2018-11-16 16:07:22 +0000

    new session code

--- current homework/lists 2 and 3.py
+++ current homework/lists 2 and 3.py
@@ -1,9 +1,6 @@
-list1 = []
 string = str(input("give list of numbers, separated my a space: "))
 list1 = list(map(int, string.split()))
 for item in list1:
-    if item%2 == 0:
+    if item % 2 == 0:
         list1.remove(item)
 print(list1)
-
-
---
+++ session 4.py
@@ -0,0 +1,3 @@
+dic1 = {'item1': 4, 'item3': 5, 'item2': 6}
+newdict = {j: i*2 for j,i in dic1.items()}
+print(newdict)

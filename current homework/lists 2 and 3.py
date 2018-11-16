5626205ccc1737a7b68682edd2ae97a4d64075df
dadato2 <dadato2@users.noreply.github.com>
2018-11-16 13:15:13 +0000

    added folder for current homeworks

---
+++ current homework/lists 2 and 3.py
@@ -0,0 +1,9 @@
+list1 = []
+string = str(input("give list of numbers, separated my a space: "))
+list1 = list(map(int, string.split()))
+for item in list1:
+    if item%2 == 0:
+        list1.remove(item)
+print(list1)
+
+

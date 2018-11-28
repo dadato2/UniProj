02348e00909e31e422ae98102a700cf51ab9f8fb
dadato2 <dadato2@users.noreply.github.com>
2018-11-28 20:04:37 +0000

    Ex 1 and 2 done

--- Homework E1.py
+++ Homework E1.py
@@ -1,9 +1,14 @@
 while True:
     a = str(input("GIve me a string: "))
     codeCount = 0
+    hicount = 0
     for char in range(0, len(a)):
         if a[char] == 'c':
             if a[char+1] == 'o':
                 if a[char+3] == 'e':
                     codeCount += 1
-    print(codeCount)
+        if a[char] == 'h':
+            if a[char+1] == 'i':
+                hicount+=1
+    print("c o [X] e :" + str(codeCount) + " times;\nh i : " + str(hicount) + " times")
+
